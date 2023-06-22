b9ed0b3ec16a86946a2ad442f45867753a383c29
Deaths-Door <aaravaditya51@gmail.com>
2023-06-22 17:11:54 +0200

    Updated Api to version 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
 pub use reqwest::*;
+pub use serde::*;
+pub use serde_json::*;
 
 pub type ParameterHashMap<'a> = HashMap<&'a str, Option<&'a str>>;
 
@@ -13,14 +15,130 @@ pub trait RequestHandler<'a> {
     /// The API key as string used for authentication.
     const API_KEY : Option<&'static str> = Some("apiKey");
 
+    /// Modifies the provided `RequestBuilder` with default headers.
+    ///
+    /// # Arguments
+    ///
+    /// * `request_builder` - The `RequestBuilder` to modify.
+    ///
+    /// # Returns
+    ///
+    /// The modified `RequestBuilder` with default headers set.
+    fn default_headers(&self,request_builder : reqwest::RequestBuilder) -> reqwest::RequestBuilder {
+        request_builder
+    }
+
+    /// Modifies the provided `RequestBuilder` with default settings for the request.
+    ///
+    /// # Arguments
+    ///
+    /// * `endpoint` - The endpoint for the request.
+    /// * `json` - The JSON payload for the request.
+    ///
+    /// # Returns
+    ///
+    /// The modified `RequestBuilder` with default settings applied.
+    fn default_requestor(&self,endpoint : &str,parameters : Option<ParameterHashMap<'a>>,json : Option<&str>) -> reqwest::RequestBuilder {
+        panic!("Function not implemented but is called");
+    }
+
+    /// Joins the given endpoint with the base URL.
+    ///
+    /// # Arguments
+    ///
+    /// * `endpoint` - The endpoint to join with the base URL.
+    ///
+    /// # Returns
+    ///
+    /// The joined URL as a `String`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    ///
+    /// struct MyRequestHandler;
+    ///
+    /// impl<'a> RequestHandler<'a> for MyRequestHandler {
+    ///     const BASE_URL: &'static str = "https://api.example.com";
+    ///
+    ///     fn make_request_url(&self, endpoint: &str) -> String {
+    ///         Self::join_endpoints(endpoint)
+    ///     }
+    /// }
+    /// ```
+    fn join_endpoints(endpoint : &str) -> String {
+        format!("{}/{}",Self::BASE_URL,endpoint)
+    }
+
     /// Builds the parameter hashmap using the given function.
+    ///
+    /// # Arguments
+    ///
+    /// * `function` - A closure that takes a mutable reference to a `ParameterHashMap` and modifies it.
+    ///
+    /// # Returns
+    ///
+    /// The populated `ParameterHashMap`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    ///
+    /// struct MyRequestHandler;
+    ///
+    /// impl<'a> RequestHandler<'a> for MyRequestHandler {
+    ///     const BASE_URL: &'static str = "https://api.example.com";
+    ///
+    ///     fn make_parameters(&self) -> ParameterHashMap<'a> {
+    ///         self.parameters(|params| {
+    ///             params.insert("key1", Some("value1"));
+    ///             params.insert("key2", Some("value2"));
+    ///         })
+    ///     }
+    /// }
+    /// ```
     fn parameters<Function>(&self,function: Function) ->  ParameterHashMap<'a> where Function : FnOnce(&mut ParameterHashMap<'a>) {
         let mut parameters : ParameterHashMap<'a> = HashMap::new();
         function(&mut parameters);
         parameters
     }
 
-    /// Sends an HTTP request with the given endpoint and parameters, and returns the parsed response.
+    /// Sends an HTTP request with the given `RequestBuilder`, and returns the parsed response.
+    ///
+    /// # Arguments
+    ///
+    /// * `request_builder` - The `RequestBuilder` containing the configured request.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the parsed response on success, or a `StatusCode` on failure.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    ///
+    /// struct MyRequestHandler;
+    ///
+    /// #[derive(serde::Deserialize)]
+    /// struct MyResponse {
+    ///     // Define your response structure here
+    /// }
+    ///
+    /// #[async_trait::async_trait]
+    /// impl<'a> RequestHandler<'a> for MyRequestHandler {
+    ///     const BASE_URL: &'static str = "https://api.example.com";
+    ///
+    ///     async fn make_request<T>(&self, request_builder: RequestBuilder) -> Result<T, StatusCode>
+    ///     where
+    ///         T: for<'de> serde::Deserialize<'de>,
+    ///     {
+    ///         self.request(request_builder).await
+    ///     }
+    /// }
+    /// ```
     async fn request<T>(request_builder : reqwest::RequestBuilder) -> std::result::Result<T,reqwest::StatusCode> where T : for<'de> serde::Deserialize<'de> {
         let response = request_builder.send().await.expect("Error requesting request");
         let status = response.status();
